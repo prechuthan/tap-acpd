f988999c3b31f9616a4e6cb1936d81b03a0609d9
Preshant Achuthan <engerium@gmail.com>
2022-09-20 20:04:01 +0800

    Initial Commit

---
+++ src/components/GradeTracker.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+function GradeTracker() {
+  return (
+    <div className="GradeTracker">
+      (TODO)
+    </div>
+  );
+}
+
+export default GradeTracker;
